7e0e41c0ef3a4502ecbc7997565531ad407b0797
OSAMAH  ALMEKHLAFI <osamah.b.ft21@mail.umy.ac.id>
2022-12-02 16:34:39 +0700

    Creating the methode to push data to the stack

--- StackUsingLinkedLidt_C/Program.cs
+++ StackUsingLinkedLidt_C/Program.cs
@@ -32,7 +32,12 @@ namespace StackUsingLinkedLidt_CSharp
             else
                 return (false);
         }
-
+        public void push(int element)
+        {
+            Node fresh;
+            fresh = new Node(element,null);
+            fresh.next = top;
+        }
 
 
 
