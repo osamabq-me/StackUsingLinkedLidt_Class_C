e159e66b77b33d98cd752ad32c3eedd5fe67410c
OSAMAH  ALMEKHLAFI <osamah.b.ft21@mail.umy.ac.id>
2022-12-02 16:27:15 +0700

    Add project files.

---
+++ StackUsingLinkedLidt_C/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackUsingLinkedLidt_CSharp
+{
+
+    class Node
+    {
+        public int info;
+        public Node next;
+        public Node(int i, Node n)
+        {
+            info = i;
+            next = n;
+        }
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
